86a61657e8b7635cba4f1025304b7c9facd64672
gebz97 <aly.m.ghobashy@gmail.com>
2024-09-30 19:38:41 +0300

    initial

---
+++ src/main/java/org/mandulis/mts/exception/GlobalExceptionHandler.java
@@ -0,0 +1,71 @@
+package org.mandulis.mts.exception;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.mandulis.mts.rest.ErrorDetails;
+import org.mandulis.mts.rest.ResponseHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({UserAlreadyExistsException.class, UserUpdateException.class})
+    public ResponseEntity<Object> badRequestHandler(RuntimeException ex, WebRequest req){
+        List<ErrorDetails> errors = new ArrayList<>();
+        errors.add(new ErrorDetails(ex.getMessage(), req.getDescription(false), LocalDateTime.now()));
+        log.error(ex.getMessage(), errors);
+        return ResponseHandler.errorResponse(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Object> unauthorizedExceptionHandler(BadCredentialsException ex, WebRequest req) {
+        List<ErrorDetails> errors = new ArrayList<>();
+        errors.add(new ErrorDetails(ex.getMessage(), req.getDescription(false), LocalDateTime.now()));
+        log.error(ex.getMessage(), errors);
+        return ResponseHandler.errorResponse(errors, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        List<ErrorDetails> errors = new ArrayList<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String errorMessage = error.getDefaultMessage();
+            String field = error.getField();
+            errors.add(new ErrorDetails(errorMessage, field, LocalDateTime.now()));
+        });
+        log.error(ex.getMessage(), errors);
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler({
+            UserNotFoundException.class,
+            GroupNotFoundException.class,
+            TicketNotFoundException.class
+    })
+    public ResponseEntity<Object> notFoundExceptionHandler(RuntimeException ex, WebRequest req){
+        List<ErrorDetails> errors = new ArrayList<>();
+        errors.add(new ErrorDetails(ex.getMessage(), req.getDescription(false), LocalDateTime.now()));
+        log.error(ex.getMessage(), errors);
+        return ResponseHandler.errorResponse(errors, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> globalExceptionHandler(Exception ex, WebRequest req){
+        List<ErrorDetails> errors = new ArrayList<>();
+        errors.add(new ErrorDetails(ex.getMessage(), req.getDescription(false), LocalDateTime.now()));
+        log.error(ex.getMessage(), errors);
+        return ResponseHandler.errorResponse(errors, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
---
+++ src/main/java/org/mandulis/mts/security/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package org.mandulis.mts.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final JwtRequestFilter jwtRequestFilter;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtRequestFilter jwtRequestFilter) throws Exception {
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/user/**").hasRole("USER")
+                        .requestMatchers("/api/v1/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .logout(LogoutConfigurer::permitAll);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
